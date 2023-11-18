2480cf2622e5961b86865684a216117fe4e3e39d
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-11-18 12:55:08 +0000

    Add project files.

---
+++ Authentication/Auth.cs
@@ -0,0 +1,16 @@
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudRP.Authentication
+{
+    internal class Auth : Script
+    {
+        [ServerEvent(Event.PlayerConnected)]
+        public void onPlayerConnected(Player player)
+        {
+            NAPI.Chat.SendChatMessageToAll($"Player {player.Name}");
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,14 @@
+﻿using GTANetworkAPI;
+using System;
+
+namespace CloudRP
+{
+    public class Main : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void Start()
+        {
+            Console.WriteLine("Gamemode started.");
+        }
+    }
+}
