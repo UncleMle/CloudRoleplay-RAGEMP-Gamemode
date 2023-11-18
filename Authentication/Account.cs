0d512178b9a9a0192b2b2fcea03c3c2b8251ea1f
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-11-18 14:33:37 +0000

    Added files

---
+++ Authentication/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using GTANetworkAPI;
+
+
+namespace CloudRP.Authentication
+{
+    internal class Account : Script
+    {
+        [Table("accounts")]
+        public class account
+        {
+            [Key]
+            [Column("id")]
+            public int account_id { get; set;}
+
+            [Column("username")]
+            public string username { get; set;}
+        }
+    }
+}
---
+++ Database/DbConn.cs
@@ -0,0 +1,23 @@
+﻿
+
+using CloudRP.Authentication;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloudRP.Database
+{
+    internal class DbConn : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost;port=3306;database=cloud_rp;user=root;password=rootadmin13;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
