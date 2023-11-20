14a73fa24a146583b2d260b8533373111fe024f7
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-11-20 16:35:51 +0000

    Vehicle position saving logic on main thread.

--- Vehicles/VehicleSystem.cs
+++ Vehicles/VehicleSystem.cs
@@ -1,15 +1,22 @@
-﻿using CloudRP.Database;
+﻿using CloudRP.Authentication;
+using CloudRP.Database;
 using GTANetworkAPI;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Timers;
 
 namespace CloudRP.Vehicles
 {
     internal class VehicleSystem : Script
     {
         public static List<DbVehicle> vehicles;
+        public static string _vehicleSharedDataIdentifier = "VehicleData";
+        private static int _timerInterval = 5000;
+        private static Timer saveVehicleTimer;
+        
 
         [ServerEvent(Event.ResourceStart)]
         public void spawnAllVehicles()
@@ -21,7 +28,7 @@ namespace CloudRP.Vehicles
 
             Console.WriteLine($"A total of {vehicles.Count} vehicles where loaded in.");
 
-            if(vehicles.Count > 0)
+            if (vehicles.Count > 0)
             {
 
                 foreach (var item in vehicles)
@@ -29,14 +36,72 @@ namespace CloudRP.Vehicles
                     spawnVehicle(item);
                 }
             };
+
+            beginSaveInterval();
         }
 
-        public static void spawnVehicle(DbVehicle vehicle)
+        void beginSaveInterval()
+        {
+            NAPI.Task.Run(() =>
+            {
+                saveVehicleTimer = new Timer();
+                saveVehicleTimer.Interval = _timerInterval;
+                saveVehicleTimer.Elapsed += saveVehiclePositions;
+
+                saveVehicleTimer.AutoReset = true;
+                saveVehicleTimer.Enabled = true;
+            });
+        }
+        
+        public static Vehicle spawnVehicle(DbVehicle vehicle)
         {
             Vector3 spawnPosition = new Vector3(vehicle.position_x, vehicle.position_y, vehicle.position_z);
             float rotation = vehicle.rotation;
 
             Vehicle veh = NAPI.Vehicle.CreateVehicle(vehicle.vehicle_spawn_hash, spawnPosition, rotation, 255, 255, "ADMIN", 255, false, true, 0);
+
+            veh.SetSharedData(_vehicleSharedDataIdentifier, vehicle);
+            veh.SetData(_vehicleSharedDataIdentifier, vehicle);
+
+            return veh;
+        }
+
+        public static void saveVehiclePositions(object source, System.Timers.ElapsedEventArgs e)
+        {
+            List<Vehicle> allVehicles =  NAPI.Pools.GetAllVehicles();
+
+            foreach (var vehicle in allVehicles)
+            {
+                try
+                {
+                    DbVehicle vehicleData = vehicle.GetData<DbVehicle>(_vehicleSharedDataIdentifier);
+                    if (vehicleData == null) return;
+                    
+                    using (DefaultDbContext dbContext = new DefaultDbContext())
+                    {
+                        DbVehicle findVehicle = dbContext.vehicles.Find(vehicleData.vehicle_id);
+                        if (findVehicle == null) return;
+
+                        Console.WriteLine(findVehicle.vehicle_name);
+
+                        if (findVehicle == null) return;
+
+                        findVehicle.position_x = vehicle.Position.X;
+                        findVehicle.position_y = vehicle.Position.Y;
+                        findVehicle.position_z = vehicle.Position.Z;
+
+                        Console.WriteLine($"Updated vehicle position for vehicle {findVehicle.vehicle_id}");
+
+                        dbContext.SaveChanges();
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
         }
     }
 }
