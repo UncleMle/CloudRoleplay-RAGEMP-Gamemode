11ae62a8f94e6699b5aac30d4cee2666a88ea0ae
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-11-20 13:12:37 +0000

    Initiated new systems.

--- client/src/client/index.ts
+++ client/src/client/index.ts
@@ -1,8 +1,10 @@
 import AdminSystem from "./AdminSystem/AdminSystem";
 import PlayerAuthentication from "./Authentication/PlayerAuthentication";
 import BrowserSystem from "./BrowserSystem/BrowserSystem";
+import NameTags from "./Nametags/Nametags";
 
 // initialize client classes.
 new PlayerAuthentication();
 new BrowserSystem();
 new AdminSystem();
+new NameTags();
