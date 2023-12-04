1e685c9c3090d4df363d38c28f54780a6ee120a3
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-12-04 09:29:00 +0000

    Improved admin reports menu styling

--- client-ui/src/components/admin/AdminReports.vue
+++ client-ui/src/components/admin/AdminReports.vue
@@ -2,46 +2,32 @@
     <main>
         <div class="fixed inset-0 z-10 w-full text-white text-lg">
             <div class="container flex items-center max-w-5xl mx-auto mt-52 border rounded-lg border-gray-400">
-                <div class="relative bg-[#0b0b0b]/70 w-full h-fit py-4 rounded-lg border border-gray-900 shadow-2xl shadow-black text-xl font-medium">
+                <div
+                    class="relative bg-[#0b0b0b]/70 w-full h-fit py-4 rounded-lg border border-gray-900 shadow-2xl shadow-black text-xl font-medium">
 
-                    <h1 class="font-bold text-2xl pb-2 pl-4"><i class="text-gray-400"></i><i class="fa-solid fa-shield text-gray-400"></i> Active Reports</h1>
+                    <h1 class="font-bold text-2xl pb-2 pl-4"><i class="text-gray-400"></i><i
+                            class="fa-solid fa-shield text-gray-400"></i> Active Reports</h1>
 
                     <CloseButton />
 
-                    <div class="p-2 ">
+                    <div class="">
                         <table class="border-separate border-spacing-4 w-full font-normal">
                             <thead>
-                              <tr >
-                                <th class="border-b-2 border-gray-500">Player</th>
-                                <th class="border-b-2 border-gray-500">Report ID</th>
-                                <th class="border-b-2 border-gray-500">Description</th>
-                                <th class="border-b-2 border-gray-500">Action</th>
-                            </tr>
+                                <tr>
+                                    <th class="border-b-2 border-gray-500">Player</th>
+                                    <th class="border-b-2 border-gray-500">Report ID</th>
+                                    <th class="border-b-2 border-gray-500">Description</th>
+                                    <th class="border-b-2 border-gray-500">Action</th>
+                                </tr>
                             </thead>
                             <tbody class="text-center">
-                                <a v-for="item in reportData" :key="item">
-                                    <tr>
-                                        <td class="border-b-2 border-gray-500">{{ item }}</td>
-                                        <td class="border-b-2 border-gray-500">Columbus</td>
-                                        <td class="border-b-2 border-gray-500">Columbus</td>
-                                        <td class="border-b-2 border-gray-500">Ohio</td>
-                                      </tr>
-                                      <tr>
-                                        <td class="border-b-2 border-gray-500">Ohio</td>
-                                        <td class="border-b-2 border-gray-500">Columbus</td>
-                                        <td class="border-b-2 border-gray-500">Columbus</td>
-                                        <td class="border-b-2 border-gray-500">Ohio</td>
-                                    </tr>
-                                      <tr>
-                                        <td class="border-b-2 border-gray-500"></td>
-                                        <td class="border-b-2 border-gray-500">Columbus</td>
-                                        <td class="border-b-2 border-gray-500">Columbus</td>
-                                        <td class="border-b-2 border-gray-500">Ohio</td>
-                                      </tr>
-                                </a>
-
+                                <tr v-for="item in reportData" :key="item">
+                                    <td class="border-b-2 border-gray-500">Player [{{ item.playerId }}]</td>
+                                    <td class="border-b-2 border-gray-500">Report [{{item.reportId}}]</td>
+                                    <td class="border-b-2 border-gray-500 max-w-xs overflow-hidden text-ellipsis">{{ item.description }}</td>
+                                </tr>
                             </tbody>
-                          </table>
+                        </table>
                     </div>
 
                 </div>
