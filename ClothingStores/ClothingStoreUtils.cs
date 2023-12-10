6318f67425aa36fcdd5c56949a81d0886ea19b49
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-12-10 03:52:36 +0000

    Base clothing store sytem.

---
+++ ClothingStores/ClothingStoreUtils.cs
@@ -0,0 +1,14 @@
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudRP.ClothingStores
+{
+    public class ClothingStore
+    {
+        public Vector3 position;
+        public string name;
+        public string displayName;
+    }
+}
