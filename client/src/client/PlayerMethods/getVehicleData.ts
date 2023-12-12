af1117a031400699a7bb6383dee62b6fe7751930
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-12-12 23:13:50 +0000

    Fixed a client side mp object expired bug

--- client/src/client/PlayerMethods/getVehicleData.ts
+++ client/src/client/PlayerMethods/getVehicleData.ts
@@ -2,6 +2,7 @@ import { _SHARED_VEHICLE_DATA } from 'Constants/Constants';
 import { VehicleData } from '../@types';
 
 const getVehicleData = (vehicle: VehicleMp): VehicleData | undefined => {
+	if(!vehicle) return;
 	let vehicleData: VehicleData | undefined = vehicle.getVariable(_SHARED_VEHICLE_DATA);
 	return vehicleData;
 }
