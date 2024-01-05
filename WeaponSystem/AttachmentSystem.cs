406da0a04b7d935f871a8921c62f46909f3a57e1
UncleMle <62445503+UncleMle@users.noreply.github.com>
2024-01-05 16:54:34 +0000

    feat: Began work on custom weapon attachment serverside.

---
+++ WeaponSystem/AttachmentSystem.cs
@@ -0,0 +1,26 @@
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CloudRP.WeaponSystem
+{
+    public class AttachmentSystem : Script
+    {
+        public static readonly string directory = Directory.GetCurrentDirectory() + "/json/";
+
+        public AttachmentSystem()
+        {
+            using (StreamReader sr = new StreamReader(directory + "weaponData.json"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+    }
+}
