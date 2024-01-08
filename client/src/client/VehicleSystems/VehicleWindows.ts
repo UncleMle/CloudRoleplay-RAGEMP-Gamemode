ca88f11a381c9d8c89a42d5c0478f9d890841b5d
UncleMle <benbassman123@gmail.com>
2024-01-08 22:13:45 +0000

    Optimized client vehicle window sync.

--- client/src/client/VehicleSystems/VehicleWindows.ts
+++ client/src/client/VehicleSystems/VehicleWindows.ts
@@ -1,34 +1,45 @@
+import { _SHARED_VEHICLE_DATA } from "@/Constants/Constants";
 import { VehicleData } from "../@types";
 import getVehicleData from "../PlayerMethods/getVehicleData";
 
 export default class VehicleWindows {
-	public static LocalPlayer: PlayerMp;
+	public static LocalPlayer: PlayerMp = mp.players.local;
 
 	constructor() {
-		VehicleWindows.LocalPlayer = mp.players.local;
-
-		mp.events.add("render", VehicleWindows.vehicleWindowSync);
+		mp.events.add("entityStreamIn", VehicleWindows.handleStreamIn);
+		mp.events.addDataHandler(_SHARED_VEHICLE_DATA, VehicleWindows.handleDataHandler);
 	}
 
-	public static vehicleWindowSync() {
-		mp.vehicles.forEachInStreamRange((veh: VehicleMp) => {
-			let vehicleData: VehicleData | undefined = getVehicleData(veh);
+	private static handleDataHandler(entity: VehicleMp, data: VehicleData) {
+		if(entity.type === "vehicle" && data !== undefined) {
+			let vehicleData: VehicleData | undefined = getVehicleData(entity as VehicleMp);
 
 			if (!vehicleData || !vehicleData.vehicle_windows) return;
 
-			vehicleData.vehicle_windows.forEach((state: string, index: number) => {
-				if (state) veh.rollDownWindow(index);
-				else if (!state) {
-					if (veh.isWindowIntact(index)) {
-						veh.fixWindow(index);
-					}
-
-					veh.rollUpWindow(index);
-				}
-			});
-
-		})
-
+			VehicleWindows.syncWindowState(entity, vehicleData);
+		}
 	}
 
+	private static handleStreamIn(entity: EntityMp) {
+		if(entity.type === "vehicle") {
+			let vehicleData: VehicleData | undefined = getVehicleData(entity as VehicleMp);
+
+			if (!vehicleData || !vehicleData.vehicle_windows) return;
+
+			VehicleWindows.syncWindowState(entity as VehicleMp, vehicleData);
+		}
+	}
+
+	private static syncWindowState(entity: VehicleMp, vehicleData: VehicleData) {
+		vehicleData.vehicle_windows.forEach((state: string, index: number) => {
+			if (state) entity.rollDownWindow(index);
+			else if (!state) {
+				if (entity.isWindowIntact(index)) {
+					entity.fixWindow(index);
+				}
+
+				entity.rollUpWindow(index);
+			}
+		});
+	}
 }
