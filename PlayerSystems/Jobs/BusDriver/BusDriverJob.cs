f779f984bafe5af132f8fb5e148c76962ca74ac2
UncleMle <benbassman123@gmail.com>
2024-01-16 17:14:42 +0000

    Bus driver serverside colshape logic & base remote event

--- PlayerSystems/Jobs/BusDriver/BusDriverJob.cs
+++ PlayerSystems/Jobs/BusDriver/BusDriverJob.cs
@@ -1,13 +1,14 @@
-﻿using GTANetworkAPI;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using CloudRP.PlayerSystems.PlayerData;
+using CloudRP.World.MarkersLabels;
+using GTANetworkAPI;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace CloudRP.PlayerSystems.Jobs.BusDriver
 {
     public class BusDriverJob
     {
+        private static string _busDepoColShapeData = "playerEnteredBusDepoColshape";
         public static List<BusDepo> busDepos = new List<BusDepo>
         {
             new BusDepo
@@ -18,5 +19,43 @@ namespace CloudRP.PlayerSystems.Jobs.BusDriver
             }
         };
 
+        public BusDriverJob()
+        {
+            busDepos.ForEach(busDepo =>
+            {
+                NAPI.Blip.CreateBlip(513, busDepo.depoStartPosition, 1.0f, 46, busDepo.depoName, 255, 2f, true, 0, 0);
+                MarkersAndLabels.setPlaceMarker(busDepo.depoStartPosition);
+                MarkersAndLabels.setTextLabel(busDepo.depoStartPosition, busDepo.depoName, 0);
+
+                ColShape depoCol = NAPI.ColShape.CreateSphereColShape(busDepo.depoStartPosition, 2f, 0);
+
+                depoCol.OnEntityEnterColShape += (col, player) =>
+                {
+                    if(col.Equals(depoCol))
+                    {
+                        player.SetCustomSharedData(_busDepoColShapeData, busDepo);
+                    }
+                };
+
+                depoCol.OnEntityExitColShape += (col, player) =>
+                {
+                    if(col.Equals(depoCol))
+                    {
+                        player.ResetSharedData(_busDepoColShapeData);
+                    }
+                };
+            });
+        }
+
+        [RemoteEvent("server:playerAttemptBusJobStart")]
+        public void startBusJob(Player player)
+        {
+            BusDepo depoData = player.GetOwnSharedData<BusDepo>(_busDepoColShapeData);
+
+            if(depoData != null)
+            {
+                Console.WriteLine("Started.");
+            }
+        }
     }
 }
