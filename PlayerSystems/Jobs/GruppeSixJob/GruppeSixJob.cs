eb3954d6e6fd07319de7da327f0c6cead81597cd
UncleMle <benbassman123@gmail.com>
2024-01-24 18:47:02 +0000

    Added job start positions

---
+++ PlayerSystems/Jobs/GruppeSixJob/GruppeSixJob.cs
@@ -0,0 +1,18 @@
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudRP.PlayerSystems.Jobs.GruppeSixJob
+{
+    public class GruppeSixJob : Script
+    {
+        private static readonly Vector3 jobStartPosition = new Vector3(-195.4, -835.3, 30.7);
+
+        public GruppeSixJob()
+        {
+            NAPI.Blip.CreateBlip(67, jobStartPosition, 1f, 2, "Gruppe Six", 255, 5f, true, 0, 0);
+        }
+
+    }
+}
---
+++ PlayerSystems/Jobs/PostalJob/PostalJob.cs
@@ -0,0 +1,18 @@
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudRP.PlayerSystems.Jobs.PostalJob
+{
+    public class PostalJob : Script
+    {
+        private static readonly Vector3 jobStartPosition = new Vector3(-232.1, -914.9, 32.3);
+
+        public PostalJob()
+        {
+            NAPI.Blip.CreateBlip(837, jobStartPosition, 1f, 62, "Postal OP", 255, 5f, true, 0, 0);
+        }
+
+    }
+}
