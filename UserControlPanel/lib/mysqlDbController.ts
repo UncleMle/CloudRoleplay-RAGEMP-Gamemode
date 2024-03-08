7c8c01cb1e26e6f9ead0d76c884cb65469f571b3
UncleMle <benbassman123@gmail.com>
2024-03-08 22:44:31 +0000

    Resolved DB Controller

--- UserControlPanel/lib/mysqlDbController.ts
+++ UserControlPanel/lib/mysqlDbController.ts
@@ -2,10 +2,10 @@ import pool from "./mysqlDb";
 
 export default class DatabaseController {
 
-    public static async selectQuery<T>(queryString: string, values: string[]): Promise<T> {
+    public static async selectQuery<T>(queryString: string, values: any[]): Promise<T> {
         console.log(`[MYSQL] SELECT Query [${queryString}] was executed with values ${values}`);
 
-        const [results] = await pool.promise().execute(queryString, values);
+        const [results] = await pool.promise().execute(queryString, values.toString());
 
         return results as T;
     }
@@ -13,7 +13,7 @@ export default class DatabaseController {
     public static async updateQuery(queryString: string, values: any[]) {
         console.log(`[MYSQL] UPDATE Query [${queryString}] was executed with values ${values}`);
 
-        await pool.promise().execute(queryString, values);
+        await pool.promise().execute(queryString, values.toString());
     }
 
 }
