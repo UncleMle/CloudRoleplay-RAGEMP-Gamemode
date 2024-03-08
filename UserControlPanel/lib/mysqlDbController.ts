d28aee394bf6bfbe7afa5d619d128343456d91e3
UncleMle <benbassman123@gmail.com>
2024-03-08 23:07:13 +0000

    More utility methods on Database Controller for UCP

--- UserControlPanel/lib/mysqlDbController.ts
+++ UserControlPanel/lib/mysqlDbController.ts
@@ -5,7 +5,7 @@ export default class DatabaseController {
     public static async selectQuery<T>(queryString: string, values: any[]): Promise<T> {
         console.log(`[MYSQL] SELECT Query [${queryString}] was executed with values ${values}`);
 
-        const [results] = await pool.promise().execute(queryString, values.toString());
+        const [results] = await pool.promise().execute(queryString, values);
 
         return results as T;
     }
@@ -13,7 +13,7 @@ export default class DatabaseController {
     public static async updateQuery(queryString: string, values: any[]) {
         console.log(`[MYSQL] UPDATE Query [${queryString}] was executed with values ${values}`);
 
-        await pool.promise().execute(queryString, values.toString());
+        await pool.promise().execute(queryString, values);
     }
 
 }
